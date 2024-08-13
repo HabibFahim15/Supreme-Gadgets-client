9c30f1df6a773d238a01ea4c4b5a90fe7b796d7a
HabibFahim15 <habibfahim71@gmail.com>
2024-08-13 21:37:16 +0600

    first commit

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../Pages/Shared/Footer/Footer";
+import Navbar from "../Pages/Shared/Navbar/Navbar";
+
+const Main = () => {
+  return (
+    <div>
+      <Navbar/>
+      <div className="min-h-[calc(100vh-145px)] md:mx-20">
+      <Outlet />
+      </div>
+      <Footer/>
+    </div>
+  );
+};
+
+export default Main;
