106657c229445fc7f67d631ae05470f6730e19eb
HabibFahim15 <habibfahim71@gmail.com>
2024-08-14 15:34:06 +0600

    3rd commit

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,4 +1,5 @@
 import Banner from "../../../Components/Home/Banner";
+import Section from "../../../Components/Home/Section";
 
 
 
@@ -6,6 +7,7 @@ const Home = () => {
   return (
     <div>
       <Banner></Banner>
+      <Section></Section>
     </div>
   );
 };
