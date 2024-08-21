83ceb492043bbbd9482790ab35b159607e39d04f
HabibFahim15 <habibfahim71@gmail.com>
2024-08-21 17:26:43 +0600

    for the details page ..single data goted

--- src/Pages/ProductDetails/ProductDetails.jsx
+++ src/Pages/ProductDetails/ProductDetails.jsx
@@ -1,13 +1,25 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const ProductDetails = () => {
   const {id} = useParams();
+  const [products, setProducts] = useState({})
   console.log(id);
+
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/products/${id}`)
+
+    .then(res => res.json())
+    .then(data => {
+      setProducts(data)
+      console.log(data);
+    })
+  },[id])
 
   return (
     <div>
-      <h1>this is product details</h1>
+      {products?.name}
     </div>
   );
 };
